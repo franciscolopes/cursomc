ae600d1f353baf92888567f9534ee94116e5e6ac
Franscico Lopes <franciscolopesdacunha@gmail.com>
2018-04-27 15:14:30 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/francisco/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/francisco/cursomc/repositories/ClienteRepository.java
@@ -1,10 +1,14 @@
 package com.francisco.cursomc.repositories;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.francisco.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository  extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
