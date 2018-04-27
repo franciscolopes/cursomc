c87cacf0b89c880af45f532740d462bf9b7e072e
Franscico Lopes <franciscolopesdacunha@gmail.com>
2018-04-27 00:48:47 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/francisco/cursomc/services/CategoriaService.java
+++ src/main/java/com/francisco/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.francisco.cursomc.domain.Categoria;
+import com.francisco.cursomc.dto.CategoriaDTO;
 import com.francisco.cursomc.repositories.CategoriaRepository;
 import com.francisco.cursomc.services.exceptions.DataIntegrityException;
 import com.francisco.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
