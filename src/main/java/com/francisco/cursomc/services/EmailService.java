731fb0e548916573b3288bb8633ca0142154b4cf
Franscico Lopes <franciscolopesdacunha@gmail.com>
2018-04-30 22:45:36 -0300

    Email com HTML

--- src/main/java/com/francisco/cursomc/services/EmailService.java
+++ src/main/java/com/francisco/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.francisco.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.francisco.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
