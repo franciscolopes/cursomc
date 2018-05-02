6a52e1599f65ff127bb31efb320b25f5555822de
Franscico Lopes <franciscolopesdacunha@gmail.com>
2018-05-02 10:53:03 -0300

    Enviando imagem via endpoint

--- src/main/java/com/francisco/cursomc/CursomcApplication.java
+++ src/main/java/com/francisco/cursomc/CursomcApplication.java
@@ -1,17 +1,13 @@
 package com.francisco.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.francisco.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,6 +15,5 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\PROJETO-PDS2\\imagens\\monte.jpg");
 	}	
 }
